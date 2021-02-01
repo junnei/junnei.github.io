a50aebd84014535dde3e3bb9b5a87f6389e63881
junnei <peinguin77@gmail.com>
2021-02-01 13:35:11 +0900

    Update hidden item
    Updated hidden item

--- next/nextjs-blog/components/Intro/line.js
+++ next/nextjs-blog/components/Intro/line.js
@@ -11,15 +11,15 @@ export default function Line({
         <h1 className="intro-hover text-6xl lg:text-8xl font-bold tracking-tighter leading-tight md:pr-2">
           {title}.
         </h1>
-        <div className="text-center md:text-right text-lg mr-4">
-          <div className="sm:mx-0 sm:hidden md:flex">
-          <Image
-            title={title}
-            src={src}
-            height={80}
-            width={width}
-            objectFit='cover'
-            />
+        <div className="flex text-center md:text-right text-lg mr-4">
+          <div className="sm:mx-0 hidden md:flex">
+            <Image
+              title={title}
+              src={src}
+              height={80}
+              width={width}
+              objectFit='cover'
+              />
           </div>
         </div>
         
